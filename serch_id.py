3efe38bbebfac349d109b22467e71961b35d5b08
ChunTze Lin <a91178xx@gmail.com>
2021-08-13 00:33:43 +0800

    Add files via upload

--- serch_id.py
+++ serch_id.py
@@ -1,59 +1,53 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
-"""
-"""
-f = open('number.txt', 'r+')
-idstring=f.read()
-idfind ='cd88'
-idpostion=idstring.find(idfind)
-f.seek(9,0)
-f.write('hello')  
-f.close()
+用來搜尋標頭修改後面字串的TOOL BY均澤 20210813
 """
 import os
 
-# path = r'C:\Users\user\Desktop'
-path = os.getcwd()
-dirs = os.listdir(path)
-print(path)
-for file in dirs:
-    if not file.endswith(".txt"):
-        continue
-    f = open(file, 'r+')
-    print("start to read txt")
-    text = []
-    for line in f:
-        text.append(line)
-    n=len(text)
-    print(n)
-    for i in range(1,n):
-        if text[i][0] == '1' and text[i][1] == '1':
-            print('找到奇怪的字')
-            text.insert(i,'11 22 33 55 66 77 99\n')
-            text.pop(i+1)
+def _main():
+    path = os.getcwd()
+    dirs = os.listdir(path)
+    serch_id(dirs)
+    print("Complete!!!Please double check!! ^_^")
+
+def serch_id(my_dirs):
+    for file in my_dirs:
+        if not file.endswith(".txt"):
+            continue
+        f = open(file, 'r+')
+        print("Start to read txt檔")
+        text = []
+        for line in f:
+            text.append(line)
+        n=len(text)
+        print('初始行數為:',n)
+        for i in range(1,n):
+            if text[i][0] == '1' and text[i][1] == '1':
+                print('第',i+1,'行找到奇怪的字')
+                text.insert(i,'11 22 33 55 66 77 99\n')
+                text.pop(i+1)
             #text.remove(i)
-        else:
-            print('沒找到')
-    f.truncate(0)
-    f.seek(0)
-    n=len(text)
-    print('現在長度',n)
-    for i in range(0,n):
-        f.writelines(text[i])        
-    f.close()
-print("complete!!!")
+            else:
+                print('沒找到')
+        f.truncate(0)
+        f.seek(0)
+        n=len(text)
+        print('現在行數一樣為:',n,"(若不一樣檔案可能有錯誤請小心檢查!)")
+        #check_all
+        for i in range(0,n):
+            print(text[i])
+        #寫入更改後的字
+        for i in range(0,n):
+            f.writelines(text[i]) 
+        
 
-# reprint("減少了一行!",n)
+        f.close()
+    
 
-"""
+if __name__ == '__main__':
+    _main()
 
-"""
 
-""""    
-def check all()
-    for i in range(0,1):
-        print(text[i])
-"""
+
+
+
